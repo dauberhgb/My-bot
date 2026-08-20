7c28640064aca1f6545319c80af06095006942ff
dauberhgb <reguluqd@gmail.com>
2026-08-20 08:03:21 +0100

    Refactor background image pasting to use alpha_composite

--- main.py
+++ main.py
@@ -279,8 +279,8 @@ async def generate_welcome_card(member, bg_url=None, lang="ar"):
       except Exception:
         bg = None
 
-    if bg:
-      base.paste(bg, (0, 0), bg)
+        if bg:
+      base = Image.alpha_composite(base, bg)
     else:
       for x in range(width):
         color = (int(30 + (x / width) * 40), int(30 + (x / width) * 20), int(60 + (x / width) * 100))
