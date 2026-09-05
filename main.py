da2fa7618cbeae79b701c7f4683db4dbe16639ee
dauberhgb <reguluqd@gmail.com>
2026-09-05 23:02:35 +0100

    Update proxy URL in main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ intents.message_content = True
 intents.members = True
 intents.voice_states = True
 
-PROXY_URL = "http://162.214.159.94:3128"
+PROXY_URL = "http://165.154.162.73:8888"
 bot = commands.Bot(command_prefix="!", intents=intents, proxy=PROXY_URL)
 violations = {}
 
