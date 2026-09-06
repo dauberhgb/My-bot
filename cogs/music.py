773d6b12d3853fd0cf4bfc25f2aa46e944e3c859
dauberhgb <reguluqd@gmail.com>
2026-09-06 03:02:26 +0100

    Fix formatting in music.py configuration

--- cogs/music.py
+++ cogs/music.py
@@ -24,7 +24,7 @@ YTDL_OPTIONS = {
     'default_search': 'ytsearch',
     'source_address': '0.0.0.0',
     'cookiefile': os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'cookies.txt')),
-    'socket_timeout': 15
+    'socket_timeout': 15,
     'extractor_args': {
         'youtube': {
             'player_client': ['web' ,'ios']
