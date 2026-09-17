f5c9b79476e0e247442cb15c11562164b2c1a309
dauberhgb <reguluqd@gmail.com>
2026-09-17 18:03:16 +0100

    Add imports for app_commands, io, database, and time

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,9 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
+import io
+import database
+import time
 
 class AdminCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
